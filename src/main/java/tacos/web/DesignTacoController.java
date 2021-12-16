e394dfb35dc1d57e447e43cdfe7488d30f4f2a62
Celox16 <sck04184@naver.com>
2021-12-16 19:01:06 +0900

    feat : ch3 JPA

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,29 +1,32 @@
 package tacos.web;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
-import tacos.Ingredient;
-import tacos.Ingredient.Type;
-import tacos.Order;
-import tacos.Taco;
-import tacos.data.IngredientRepository;
-import tacos.data.TacoRepository;
-
-import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.extern.slf4j.Slf4j;
+import tacos.Taco;
+import tacos.Order;
+import tacos.Ingredient;
+import tacos.Ingredient.Type;
+import tacos.data.IngredientRepository;
+import javax.validation.Valid;
+import org.springframework.validation.Errors;
+import tacos.data.TacoRepository;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
 @SessionAttributes("order")
 public class DesignTacoController {
-
     private final IngredientRepository ingredientRepo;
 
     private TacoRepository tacoRepo;
@@ -36,18 +39,26 @@ public class DesignTacoController {
 
     @GetMapping
     public String showDesignForm(Model model) {
-
         List<Ingredient> ingredients = new ArrayList<>();
         ingredientRepo.findAll().forEach(i -> ingredients.add(i));
 
         Type[] types = Ingredient.Type.values();
-        for(Type type : types) {
-            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
         }
 
         model.addAttribute("taco", new Taco());
 
         return "design";
+    }
+
+    private List<Ingredient> filterByType(
+            List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
     }
 
     @ModelAttribute(name = "order")
@@ -66,14 +77,9 @@ public class DesignTacoController {
             return "design";
         }
 
-        // 이 지점에서 타코 디자인(선택된 식자재 내역)을 저장한다.
         Taco saved = tacoRepo.save(design);
         order.addDesign(saved);
 
         return "redirect:/orders/current";
-    }
-
-    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
-        return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
     }
 }
