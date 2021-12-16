3633b57eba41203e515a3a339ef3c0ab417b3a4d
Celox16 <sck04184@naver.com>
2021-12-16 14:34:26 +0900

    feat : make simple jdbc inserter

---
+++ src/main/java/tacos/data/JdbcOrderRepository.java
@@ -0,0 +1,26 @@
+package tacos.data;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JdbcOrderRepository {
+    private SimpleJdbcInsert orderInserter;
+    private SimpleJdbcInsert orderTacoInserter;
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    public JdbcOrderRepository(JdbcTemplate jdbc) {
+        this.orderInserter = new SimpleJdbcInsert(jdbc)
+                .withTableName("Taco_Order")
+                .usingGeneratedKeyColumns("id");
+
+        this.orderTacoInserter = new SimpleJdbcInsert(jdbc)
+                .withTableName("Taco_Order_Tacos");
+
+        this.objectMapper = new ObjectMapper();
+    }
+}
